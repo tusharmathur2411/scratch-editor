6bbc685604c4d3797ec32a4950057900c7d49dd5
tusharmathur2411 <tusharmathur2411@gmail.com>
2021-07-11 14:57:09 +0530

    Added motion buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+// import sprite from './sprite.png';
+import './styles/App.css';
+import { useState, useEffect, useRef } from 'react';
+import Motion from './Components/Motion';
+import CatSprite from './Components/CatSprite'
 
 function App() {
+
+  const [position, setPosition] = useState({x: 0, y: 0, direction: 90})
+  const sprite1 = useRef()
+
+  const updatePosition = () => 
+    setPosition(d =>{
+      const tr = sprite1.current.style.transform.split("(")
+      const deg = parseInt(tr[tr.length-1])||0
+      return {
+        ...d,
+        x: parseInt(sprite1.current.style.left || 0),
+        y: parseInt(sprite1.current.style.top || 0),
+        direction: 90+ deg
+      }
+      
+    }
+  )  
+  
+  useEffect(() => {
+    updatePosition()
+  }, [sprite1])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="code">
+        <Motion sprite={sprite1.current} setPosition={updatePosition} />
+
+      </div>
+      <div>
+        <div className="canvas-header">
+        <img src="https://img.icons8.com/material-rounded/24/fa314a/flag.png" alt="" />
+        </div>
+        <div className="canvas">
+          <div className="i-canvas">
+            {/* <img src={sprite} ref={sprite1} alt="sprite" id="sprite1" /> */}
+            <CatSprite ref={sprite1} id="sprite1" />
+          </div>
+        </div>
+        <div>
+          <b>x: {position.x}</b><br/>
+          <b>y: {position.y*-1}</b><br/>
+          <b>Direction: {position.direction}</b><br/>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Motion.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react'
+import "../styles/Motion.css"
+
+const Motion = ({id, setPosition, sprite}) => {
+
+    const [steps, setSteps] = useState(10)
+    const [clock, setClock] = useState(15)
+    const [counterClock, setCounterClock] = useState(15)
+    const [myPos, setMyPos] = useState({x:0, y:0})
+
+    const toRad = angle => {
+        return angle * (Math.PI / 180);
+    }
+
+    useEffect(() => {
+        var observer = new MutationObserver(mutations => {
+            mutations.forEach(mutationRecord => {
+                setPosition()
+            });    
+        });        
+        var target = document.getElementById("sprite1");
+        observer.observe(target, { attributes : true, attributeFilter : ['style'] });
+    }, [id, setPosition])
+
+    const handleInputChange = e => ["-", ""].includes(e.target.value)?e.target.value:Number(e.target.value)
+
+    const onMove = e => {
+        if (e.target.tagName === "INPUT") return
+        const tr = sprite.style.transform.split("(")
+        const angle = toRad(parseInt(tr[tr.length-1])||0)
+        const x = parseInt(sprite.style.left) || 0
+        const y = parseInt(sprite.style.top) || 0
+        const xd = Math.cos(angle) * steps
+        const yd = Math.sin(angle) * steps
+        sprite.style.left = `${x+xd}px`
+        sprite.style.top = `${y+yd}px`
+    }
+
+    const onTurn = (e, dir) => {
+        if (e.target.tagName === "INPUT") return
+        const tr = sprite.style.transform.split("(")
+        const deg = parseInt(tr[tr.length-1])||0
+        sprite.style.transform = `translate(-50%,-50%) rotate(${dir==="+"?deg+clock:deg-counterClock}deg)`
+    }
+
+    const goToPos = (e, x, y) => {
+        if (e.target.tagName === "INPUT") return
+        sprite.style.left = `${x}px`
+        sprite.style.top = `${y}px`
+    }
+
+    return (
+        <>
+            <h2>Motion</h2>
+            <div className="motion" onClick={onMove}>
+                Move<input value={steps} onChange={e => setSteps(handleInputChange(e))} onClick={null} />steps
+            </div>
+            <div className="motion" onClick={e => onTurn(e, "+")}>
+                Turn <img src="https://img.icons8.com/fluent/25/ffffff/restart.png" alt=""/> <input value={clock} onChange={e => setClock(handleInputChange(e))} onClick={null} /> degrees
+            </div>
+            <div className="motion" onClick={e => onTurn(e, "-")}>
+                Turn <img style={{transform: "rotateY(180deg)"}} src="https://img.icons8.com/fluent/25/ffffff/restart.png" alt=""/> <input value={counterClock} onChange={e => setCounterClock(handleInputChange(e))} onClick={null} /> degrees
+            </div>
+            <div className="motion" onClick={e => goToPos(e, myPos.x, myPos.y)}>
+                Go to x: 
+                <input value={myPos.x} onChange={e => setMyPos(p => ({...p, x:handleInputChange(e)}))} onClick={null} />
+                y: 
+                <input value={myPos.y} onChange={e => setMyPos(p => ({...p, y:handleInputChange(e)}))} onClick={null} />
+            </div>
+        </>
+    )
+}
+
+export default Motion
